dc3d63c1e2fc61d54b948738d5608072438ee44c
Bedagya-Joshi <5h4d0mf0rc3@gmail.com>
2023-12-05 16:40:51 +0545

    misc

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,3 +33,21 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch((error) => console.error("Error fetching navbar:", error));
 });
+
+function handleScroll() {
+    const sections = document.querySelectorAll(".fade-in");
+    sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (sectionTop < windowHeight - 150) {
+        section.classList.add("active");
+        } else {
+        section.classList.remove("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", handleScroll);
+
+handleScroll();
